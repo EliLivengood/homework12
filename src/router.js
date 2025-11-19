05b6ee667dd5c14e089380d960b868bed63af044
EliLivengood <elivengo@stu.southtexascollege.edu>
2025-11-19 15:28:54 -0600

    Navigation is working

--- src/router.js
+++ src/router.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
+import MealsByName from './views/MealsByName.vue'
+import MealsByLetter from './views/MealsByLetter.vue'
+import MealsByIngredients from './views/MealsByIngredients.vue'
+import MealDetails from './views/MealDetails.vue'
+import Ingredients from './views/Ingredients.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
+  { path: '/homework12/', name: 'Home', component: HomePage },
+  { path: '/homework12/by-name/:name?', name: 'MealsByName', component: MealsByName },
+  { path: '/homework12/by-letter/:letter?', name: 'MealsByLetter', component: MealsByLetter },
+  { path: '/homework12/by-ingredients/:ingredient?', name: 'MealsByIngredients', component: MealsByIngredients },
+  { path: '/homework12/meal/:id', name: 'MealDetails', component: MealDetails },
+  { path: '/homework12/ingredients', name: 'Ingredients', component: Ingredients },
   { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
 ]
 
