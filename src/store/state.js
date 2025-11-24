b5e04443cfc57a026900cfb65fe593510009ab23
EliLivengood <elivengo@stu.southtexascollege.edu>
2025-11-24 09:10:08 -0600

    Data store and Meals by letter complete

---
+++ src/store/state.js
@@ -0,0 +1,9 @@
+import Ingredients from "@/views/Ingredients.vue";
+import MealsByIngredients from "@/views/MealsByIngredients.vue";
+
+export default {
+    searchedMeals: [],
+    mealsByLetter: [],
+    MealsByIngredient: [],
+    ingredient: {}
+}
